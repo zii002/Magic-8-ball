7673ebee76e14c4a67bb86f14e411fd273e371d7
Zeynep Akkurt <zeynepakkurt@gmail.com>
2026-03-08 16:11:17 +0300

    Add Magic 8-Ball functionality to magic8.py
    Implemented a Magic 8-Ball game that provides random answers to a user's question.

--- magic8.py
+++ magic8.py
@@ -1,1 +1,28 @@
+import random
+name = "Zeynep"
+random_question=("Am i gonna be successful?:")
+answer_number=random.randint(1,9)
 
+if answer_number==1:
+  answer="Yes Definitely"
+elif answer_number ==2:
+  answer="It is decidedly so"
+elif answer_number==3:
+  answer="Without a doubt"
+elif answer_number==4:
+  answer="Reply hazy,try again"
+elif answer_number==5:
+  answer="ask again later"
+elif answer_number==6:
+  answer="Better not tell you now"
+elif answer_number==7:
+  answer="my sources say no"
+elif answer_number==8:
+  answer="Outlook not so good"
+elif answer_number==9:
+  answer="Very doubtful"
+else:
+  print("Error!")
+
+print(str(name)+ " asks : "+ random_question )
+print("Magic 8-Ball's answer: " + answer)
